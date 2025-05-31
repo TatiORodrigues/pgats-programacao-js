70522b660f2b0e0ba3438d7bb8ac85d0232bf2b3
TatiORodrigues <136990766+TatiORodrigues@users.noreply.github.com>
2025-05-31 20:39:12 +0100

    Update trabalho.js

--- trabalho.js
+++ trabalho.js
@@ -1,3 +1,35 @@
-function geradorDeTagsDeIdentificacao(nome) {
-  return 'PANTERA'
+//function geradorDeTagsDeIdentificacao(nome) {
+  //return 'PANTERA'
+//}
+
+export function geradorDeTagsDeIdentificacao(nome) {
+  return nome.toUpperCase();
+}
+
+export function verificarSePodeSerAdotado(idade, porte) {
+  return idade >= 1 && porte === 'M';
+}
+
+export function calcularConsumoDeRacao(nome, idade, peso) {
+  // Supondo: cada kg = 300g de ração
+  // Para 14.5 kg -> 14.5 * 300 = 4350g
+  return peso * 300;
+}
+
+export function decidirTipoDeAtividadePorPorte(porte) {
+  const atividades = {
+    pequeno: 'brincar dentro de casa',
+    medio: 'caminhadas diárias',
+    grande: 'atividades ao ar livre'
+  };
+  return atividades[porte.toLowerCase()];
+}
+
+export async function buscarDadoAsync() {
+  // Simulando uma função assíncrona com Promise
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Pipoca');
+    }, 100);
+  });
 }
